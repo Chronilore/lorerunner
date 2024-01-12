25529f03a3d2d6a143370d6be54586b78ed3f90b
Sean Myers <spmyers@protonmail.com>
2024-01-12 08:37:32 -0500

    updating print statement for launch template

--- src/amazon_web_services/ec2.rs
+++ src/amazon_web_services/ec2.rs
@@ -13,23 +13,10 @@ pub async fn get_ec2_launch_templates(client: aws_sdk_ec2::Client) -> Result<()>
         .send()
         .await?;
 
-    result.launch_templates().iter().for_each(|template| {
-        println!(
-            "Launch Template:\nid: {:<12} name: {:<10} latest_version_number: {:<4}",
-            template
-                .launch_template_id
-                .as_ref()
-                .map_or_else(|| "?".to_string(), |id| id.to_string()),
-            template
-                .launch_template_name
-                .as_ref()
-                .map_or_else(|| "?".to_string(), |name| name.to_string()),
-            template
-                .latest_version_number
-                .as_ref()
-                .map_or_else(|| &-1i64, |number| number)
-        )
-    });
+    result
+        .launch_templates()
+        .iter()
+        .for_each(|template| println!("Launch Template:\n{:?}", template));
 
     Ok(())
 }
